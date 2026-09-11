2a170b61a4f31e0927afe0b58b0ee7a51e123bee
GraceWang <grace.wang@suse.com>
2026-09-11 15:30:42 +0800

    Remove softffailure for boo1074057 and fix poo206652

--- tests/x11/gnomecase/nautilus_permission.pm
+++ tests/x11/gnomecase/nautilus_permission.pm
@@ -18,20 +18,21 @@
 
 use Mojo::Base 'x11test';
 use testapi;
-use version_utils qw(is_sle is_leap is_tumbleweed);
+# use version_utils qw(is_sle is_leap is_tumbleweed);
 
 
 sub run {
     x11_start_program('touch newfile', valid => 0);
     x11_start_program('nautilus');
     send_key_until_needlematch 'nautilus-newfile-matched', 'right', 16;
-    if (is_sle('15+') || is_leap('15.0+') || is_tumbleweed) {
-        assert_and_click('nautilus-newfile-matched', button => 'right');
-        record_soft_failure 'boo#1074057 qemu can not properly capture some keys in nautilus under GNOME wayland';
-    }
-    else {
-        send_key "shift-f10";
-    }
+    # if (is_sle('15+') || is_leap('15.0+') || is_tumbleweed) {
+    #     assert_and_click('nautilus-newfile-matched', button => 'right');
+    #     record_soft_failure 'boo#1074057 qemu can not properly capture some keys in nautilus under GNOME wayland';
+    # }
+    # else {
+    #     send_key "shift-f10";
+    # }
+    assert_and_click('nautilus-newfile-matched', button => 'right');
     assert_screen 'nautilus-rightkey-menu';
     send_key "r";    #choose properties
     assert_screen 'nautilus-properties';
@@ -43,12 +44,13 @@ sub run {
     send_key "esc";    #close the custom permissions dialog
     send_key "esc";    #close the dialog
                        #reopen the properties menu to check if the changes kept
-    if (is_sle('15+') || is_leap('15.0+') || is_tumbleweed) {
-        assert_and_click('nautilus-newfile-matched', button => 'right');
-    }
-    else {
-        send_key "shift-f10";
-    }
+    # if (is_sle('15+') || is_leap('15.0+') || is_tumbleweed) {
+    #     assert_and_click('nautilus-newfile-matched', button => 'right');
+    # }
+    # else {
+    #     send_key "shift-f10";
+    # }
+    assert_and_click('nautilus-newfile-matched', button => 'right');
     assert_screen 'nautilus-rightkey-menu';
     send_key "r";    #choose properties
     assert_screen 'nautilus-properties';
